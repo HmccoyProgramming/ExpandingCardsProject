12f24fe549a3b2a860c5c15bef9c9869122bcf7b
HmccoyProgramming <88503108+HmccoyProgramming@users.noreply.github.com>
2021-12-03 13:21:06 -0800

    Add files via upload

---
+++ Expanding Cards/script.js
@@ -0,0 +1,28 @@
+// document.querySelector('h1').style.color = 'pink';
+
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach((panel) =>
+    panel.addEventListener('click', function() {
+        if (panel.classList.contains('active')) {
+            panel.classList.remove('active')
+        }else {
+       removeActiveClasses()
+        
+       panel.classList.add('active')
+        }
+       
+    
+
+
+
+
+    }))
+
+
+
+
+function removeActiveClasses() {
+    panels.forEach(panel => panel.classList.remove('active'))
+}
